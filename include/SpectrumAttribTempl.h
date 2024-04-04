bc66c78bbc5e0c6b14a8d30fedd1a18e499f0b57
Jens Georg <jens.georg@desy.de>
2024-04-04 13:38:24 +0000

    fix: Clean-up memory handling
    We cannot assure that the buffer in the PV lives long enough for the
    CORBA call, so we have to copy it before sending it into Tango

--- include/SpectrumAttribTempl.h
+++ include/SpectrumAttribTempl.h
@@ -5,10 +5,10 @@
 #include <ChimeraTK/NDRegisterAccessor.h>
 
 namespace ChimeraTK {
-  template<typename T>
+  template<typename TangoType, typename AdapterType>
   class SpectrumAttribTempl : public Tango::SpectrumAttr, Tango::LogAdapter {
    public:
-    SpectrumAttribTempl(TANGO_BASE_CLASS* tangoDevice, boost::shared_ptr<ChimeraTK::NDRegisterAccessor<T>> pv,
+    SpectrumAttribTempl(TANGO_BASE_CLASS* tangoDevice, boost::shared_ptr<ChimeraTK::NDRegisterAccessor<AdapterType>> pv,
         std::shared_ptr<AttributProperty> attProperty)
     : Tango::SpectrumAttr(
           attProperty->name.c_str(), attProperty->dataType, attProperty->writeType, pv->getNumberOfSamples()),
@@ -31,42 +31,32 @@ namespace ChimeraTK {
         axis_prop.set_unit(attProperty->unit.c_str());
       }
 
-      if constexpr(std::is_same<T, std::string>::value) {
-        attrStringRead.reserve(length);
-      }
-      else if(dataType == Tango::DEV_BOOLEAN) {
-        // FIXME: Find a nicer way than plain pointers
-        attrBoolRead = new Tango::DevBoolean[length];
-      }
       set_default_properties(axis_prop);
     }
 
-    ~SpectrumAttribTempl() override { delete[] attrBoolRead; };
+    ~SpectrumAttribTempl() override = default;
 
     void read([[maybe_unused]] Tango::DeviceImpl* dev, Tango::Attribute& att) override {
       DEBUG_STREAM << "SpectrumAttribTempl::read " << get_name() << std::endl;
 
-      if constexpr(std::is_same<T, std::string>::value) {
-        for(unsigned int i = 0; i < length; i++) {
-          // char * is required by the Tango API. The data is copied internally
-          // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
-          attrStringRead[i] = const_cast<char*>(processSpectrum->accessData(i).c_str());
-        }
-        att.set_value(attrStringRead.data(), length);
-      }
-      else if(dataType == Tango::DEV_BOOLEAN) {
-        auto pv = boost::reinterpret_pointer_cast<ChimeraTK::NDRegisterAccessor<Boolean>>(processSpectrum);
-        for(unsigned int i = 0; i < length; i++) {
-          attrBoolRead[i] = processSpectrum->accessData(i);
-        }
+      TangoType* data = new TangoType[length];
 
-        att.set_value(attrBoolRead, length);
+      for(unsigned int i = 0; i < length; i++) {
+        if constexpr(std::is_same_v<TangoType, Tango::DevString>) {
+          data[i] = Tango::string_dup(processSpectrum->accessData(i).c_str());
+        }
+        else {
+          data[i] = processSpectrum->accessData(i);
+        }
+      }
+      att.set_value(data, length, 0, true);
+
+      if(processSpectrum->dataValidity() != ChimeraTK::DataValidity::ok) {
+        att.set_quality(Tango::AttrQuality::ATTR_INVALID);
+        ERROR_STREAM << "SprectrumAttribTempl::read " << get_name() << " is not valid" << std::endl;
       }
       else {
-        att.set_value(processSpectrum->accessChannel(0).data(), length);
-      }
-      if(processSpectrum->dataValidity() != ChimeraTK::DataValidity::ok) {
-        ERROR_STREAM << "SpectrumAttribTempl::read " << get_name() << " is not valid" << std::endl;
+        att.set_quality(Tango::AttrQuality::ATTR_VALID);
       }
     }
 
@@ -86,172 +76,71 @@ namespace ChimeraTK {
         Tango::Except::throw_exception("WRITE_ERROR", msg.str(), "SpectrumAttribTempl::write()");
       }
 
-      switch(dataType) {
-        case Tango::DEV_UCHAR: {
-          const uint8_t* c_value;
-          if(att.get_user_set_write_value()) {
-            c_value = att.get_last_written_uch()->get_buffer();
-          }
-          else {
-            att.get_write_value(c_value);
-            memoried_value = arrayToString(c_value, length);
-          }
+      const TangoType* data = nullptr;
+      if(att.get_user_set_write_value()) {
+        if constexpr(std::is_same_v<Tango::DevUChar, TangoType>) {
+          data = att.get_last_written_uch()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevUShort, TangoType>) {
+          data = att.get_last_written_ush()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevULong, TangoType>) {
+          data = att.get_last_written_ulg()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevULong64, TangoType>) {
+          data = att.get_last_written_ulg64()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevShort, TangoType>) {
+          data = att.get_last_written_sh()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevLong, TangoType>) {
+          data = att.get_last_written_lg()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevLong64, TangoType>) {
+          data = att.get_last_written_lg64()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevFloat, TangoType>) {
+          data = att.get_last_written_fl()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevDouble, TangoType>) {
+          data = att.get_last_written_db()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevBoolean, TangoType>) {
+          data = att.get_last_written_boo()->get_buffer();
+        }
+        else if constexpr(std::is_same_v<Tango::DevString, TangoType>) {
+          data = att.get_last_written_str()->get_buffer();
+        }
+        else {
+          std::cerr << "Unsupported tango type" << std::endl;
+          assert(false);
+        }
+      }
+      else {
+        if constexpr(std::is_same_v<Tango::DevString, TangoType>) {
+          // Need to cast to const Tango::ConstDevString* here because otherwise Tangos
+          // internal type dispatch thinks it's an array of enums.
+          auto* dataCasted = reinterpret_cast<const Tango::ConstDevString*>(data);
+          att.get_write_value(dataCasted);
+        }
+        else {
+          att.get_write_value(data);
+        }
+        memoried_value = arrayToString(data, length);
+      }
 
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = c_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_USHORT: {
-          const Tango::DevUShort* us_value;
-          if(att.get_user_set_write_value()) {
-            us_value = att.get_last_written_ush()->get_buffer();
-          }
-          else {
-            att.get_write_value(us_value);
-            memoried_value = arrayToString(us_value, length);
-          }
+      if constexpr(std::is_same_v<TangoType, Tango::DevString>) {
+        std::transform(data, data + arraySize, processVector.begin(), [](auto v) { return std::string(v); });
+      }
+      else {
+        std::copy(data, data + arraySize, processVector.begin());
+      }
 
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = us_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_ULONG: {
-          const uint32_t* ulg_value;
-          if(att.get_user_set_write_value()) {
-            ulg_value = att.get_last_written_ulg()->get_buffer();
-          }
-          else {
-            att.get_write_value(ulg_value);
-            memoried_value = arrayToString(ulg_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = ulg_value[i];
-          }
-          break;
-        }
-
-        case Tango::DEV_ULONG64: {
-          const uint64_t* ulg64_value;
-          if(att.get_user_set_write_value()) {
-            ulg64_value = att.get_last_written_ulg64()->get_buffer();
-          }
-          else {
-            att.get_write_value(ulg64_value);
-            memoried_value = arrayToString(ulg64_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = ulg64_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_SHORT: {
-          const int16_t* sh_value;
-          if(att.get_user_set_write_value()) {
-            sh_value = att.get_last_written_sh()->get_buffer();
-          }
-          else {
-            att.get_write_value(sh_value);
-            memoried_value = arrayToString(sh_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = sh_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_LONG: {
-          const int32_t* lg_value;
-          if(att.get_user_set_write_value()) {
-            lg_value = att.get_last_written_lg()->get_buffer();
-          }
-          else {
-            att.get_write_value(lg_value);
-            memoried_value = arrayToString(lg_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = lg_value[i];
-          }
-
-          break;
-        }
-        case Tango::DEV_LONG64: {
-          const int64_t* lg64_value;
-          if(att.get_user_set_write_value()) {
-            lg64_value = att.get_last_written_lg64()->get_buffer();
-          }
-          else {
-            att.get_write_value(lg64_value);
-            memoried_value = arrayToString(lg64_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = lg64_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_FLOAT: {
-          const float* fl_value;
-          if(att.get_user_set_write_value()) {
-            fl_value = att.get_last_written_fl()->get_buffer();
-          }
-          else {
-            att.get_write_value(fl_value);
-            memoried_value = arrayToString(fl_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = fl_value[i];
-          }
-          break;
-        }
-
-        case Tango::DEV_DOUBLE: {
-          const double* db_value;
-          if(att.get_user_set_write_value()) {
-            db_value = att.get_last_written_db()->get_buffer();
-          }
-          else {
-            att.get_write_value(db_value);
-            memoried_value = arrayToString(db_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = db_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_BOOLEAN: {
-          const Tango::DevBoolean* bool_value;
-          if(att.get_user_set_write_value()) {
-            bool_value = att.get_last_written_boo()->get_buffer();
-          }
-          else {
-            att.get_write_value(bool_value);
-            memoried_value = arrayToString(bool_value, length);
-          }
-          for(size_t i = 0; i < arraySize; ++i) {
-            processVector[i] = bool_value[i];
-          }
-          break;
-        }
-        case Tango::DEV_STRING: {
-          if constexpr(std::is_same<T, std::string>::value) {
-            const Tango::ConstDevString* str_value;
-            if(att.get_user_set_write_value()) {
-              str_value = att.get_last_written_str()->get_buffer();
-            }
-            else {
-              att.get_write_value(str_value);
-              memoried_value = arrayToString(str_value, length);
-            }
-
-            for(size_t i = 0; i < arraySize; ++i) {
-              processVector[i] = std::string(str_value[i]);
-            }
-          }
-          break;
-        }
-        default:
-          ERROR_STREAM << " SpectrumAttribTempl::write " << get_name() << " unsupported dataTye " << dataType
-                       << std::endl;
+      if(att.get_quality() == Tango::AttrQuality::ATTR_INVALID) {
+        processSpectrum->setDataValidity(DataValidity::faulty);
+      }
+      else {
+        processSpectrum->setDataValidity(DataValidity::ok);
       }
 
       try {
@@ -280,7 +169,7 @@ namespace ChimeraTK {
       return true;
     }
 
-    boost::shared_ptr<ChimeraTK::NDRegisterAccessor<T>> processSpectrum;
+    boost::shared_ptr<ChimeraTK::NDRegisterAccessor<AdapterType>> processSpectrum;
     Tango::CmdArgType dataType;
     unsigned int length;
     std::vector<Tango::DevString> attrStringRead{};
