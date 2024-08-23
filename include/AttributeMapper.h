2095e79dfe8acf4dd02b31171c90bb8d9315a3df
Jens Georg <jens.georg@desy.de>
2024-08-23 14:34:49 +0000

    fix: Properly map different devices

--- include/AttributeMapper.h
+++ include/AttributeMapper.h
@@ -2,7 +2,6 @@
 #include <ChimeraTK/ControlSystemAdapter/ControlSystemPVManager.h>
 
 #include <AttributeProperty.h>
-#include <iostream>
 #include <list>
 #include <set>
 
@@ -13,12 +12,12 @@ namespace ChimeraTK {
     AttributeMapper() = default;
     AttributeMapper(AttributeMapper&) = delete;
     void operator=(AttributeMapper const&) = delete;
-    void directImport(std::set<std::string>& inputVariables);
-    void prepareOutput(std::vector<std::shared_ptr<ChimeraTK::AttributeProperty>>& attributeList);
+    void directImport(std::set<std::string>& inputVariables, std::string device);
+    void prepareOutput(std::vector<std::shared_ptr<ChimeraTK::AttributeProperty>>& attributeList, std::string device);
     [[nodiscard]] const std::set<std::string>& getUsedVariables() const { return _usedInputVariables; }
     // empty the created mapping
     void clear();
-    [[nodiscard]] std::list<std::shared_ptr<AttributeProperty>> const& getAttDescList() const;
+    [[nodiscard]] std::list<std::shared_ptr<AttributeProperty>> const& getAttDescList(std::string device) const;
     void import(std::string importSource, const std::string& importLocationName, const std::string& directory = "");
     void deriveType(std::type_info const& info);
     void setCSPVManager(boost::shared_ptr<ControlSystemPVManager> csPVManager) {
@@ -30,7 +29,7 @@ namespace ChimeraTK {
     std::set<std::string> _inputVariables;
     std::set<std::string> _usedInputVariables; // For tracing which variables are
                                                // not to be imported.
-    std::list<std::shared_ptr<AttributeProperty>> _descriptions;
+    std::map<std::string, std::list<std::shared_ptr<AttributeProperty>>> _descriptions;
     boost::shared_ptr<ControlSystemPVManager> _controlSystemPVManager;
     Tango::CmdArgType _dataType{Tango::CmdArgType::DATA_TYPE_UNKNOWN};
   };
